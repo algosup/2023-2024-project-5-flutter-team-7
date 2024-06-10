2c4b37aef9f320decd1b24c40536b76bc696f6b0
JasonGROSSO <114397870+JasonGROSSO@users.noreply.github.com>
2024-06-10 15:40:02 +0200

    create pages
    sign in page for company
    account page for company
    modified the routes to include the new pages

--- src/adopteuncandidat/lib/routes.dart
+++ src/adopteuncandidat/lib/routes.dart
@@ -1,3 +1,6 @@
+import 'package:adopteuncandidat/recruiter_profile.dart';
+import 'package:adopteuncandidat/sign_in_recruiter.dart';
+
 import 'hobbies_selection.dart';
 import 'job_seeker_profile.dart';
 import 'login.dart';
@@ -10,85 +13,99 @@ import 'matchmaking_done.dart';
 import 'package:go_router/go_router.dart';
 import 'package:flutter/material.dart';
 
-
-final GoRouter router = GoRouter(routes: <RouteBase>[
-  GoRoute(
-    name: 'signIn',
-    path: '/signIn',
-    builder: (BuildContext context, GoRouterState state) {
-      return const SignInScreen();
-    },
-  ),
-  GoRoute(
-    name: 'home',
-    path: '/',
-    builder: (BuildContext context, GoRouterState state) {
-      return const StartScreen();
-    },
-  ),
-  GoRoute(
-    name: 'signInSeeker',
-    path: '/signInSeeker',
-    builder: (BuildContext context, GoRouterState state) {
-      return const SignInSeekerScreen();
-    },
-  ),
-  GoRoute(
-    name: 'matchmaking',
-    path: '/matchmaking',
-    builder: (BuildContext context, GoRouterState state) {
-      return const MatchmakingScreen();
-    },
-  ),
-  GoRoute(
-    name: 'matchmakingDone',
-    path: '/matchmakingDone',
-    builder: (BuildContext context, GoRouterState state) {
-      return const MatchmakingDoneScreen();
-    },
-  ),
-  GoRoute(
-    name: 'login',
-    path: '/login',
-    builder: (BuildContext context, GoRouterState state) {
-      return const LoginScreen();
-    },
-  ),
-  GoRoute(
-    name: 'softSkillsSeeker',
-    path: '/softSkillsSeeker',
-    builder: (BuildContext context, GoRouterState state) {
-      return const SoftSkillsSelectionScreen(isEditMode: false);
-    },
-  ),
-  GoRoute(
-    name: 'hobbies',
-    path: '/hobbies',
-    builder: (BuildContext context, GoRouterState state) {
-      return const HobbiesSelectionScreen(isEditMode: false,);
-    },
-  ),
-
-  GoRoute(
-    name: 'editHobbies',
-    path: '/editHobbies',
-    builder: (BuildContext context, GoRouterState state) {
-      return const HobbiesSelectionScreen(isEditMode: true);
-    },
-  ),
-  GoRoute(
-    name: 'jobSeekerProfile',
-    path: '/jobSeekerProfile',
-    builder: (BuildContext context, GoRouterState state) {
-      return const JobSeekerScreen();
-    },
-  ),
-
-GoRoute(
-  path: '/editSoftSkillsSeeker',
-  builder: (BuildContext context, GoRouterState state) {
-    return const SoftSkillsSelectionScreen(isEditMode: true);
-  },
-),
-
-]);
+final GoRouter router = GoRouter(
+  routes: <RouteBase>[
+    GoRoute(
+      name: 'signIn',
+      path: '/signIn',
+      builder: (BuildContext context, GoRouterState state) {
+        return const SignInScreen();
+      },
+    ),
+    GoRoute(
+      name: 'home',
+      path: '/',
+      builder: (BuildContext context, GoRouterState state) {
+        return const StartScreen();
+      },
+    ),
+    GoRoute(
+      name: 'signInSeeker',
+      path: '/signInSeeker',
+      builder: (BuildContext context, GoRouterState state) {
+        return const SignInSeekerScreen();
+      },
+    ),
+    GoRoute(
+      name: 'signInRecruiter',
+      path: '/signinRecruiter',
+      builder: (BuildContext context, GoRouterState state) {
+        return const SignInRecruiterScreen();
+      },
+    ),
+    GoRoute(
+      name: 'matchmaking',
+      path: '/matchmaking',
+      builder: (BuildContext context, GoRouterState state) {
+        return const MatchmakingScreen();
+      },
+    ),
+    GoRoute(
+      name: 'matchmakingDone',
+      path: '/matchmakingDone',
+      builder: (BuildContext context, GoRouterState state) {
+        return const MatchmakingDoneScreen();
+      },
+    ),
+    GoRoute(
+      name: 'login',
+      path: '/login',
+      builder: (BuildContext context, GoRouterState state) {
+        return const LoginScreen();
+      },
+    ),
+    GoRoute(
+      name: 'softSkillsSeeker',
+      path: '/softSkillsSeeker',
+      builder: (BuildContext context, GoRouterState state) {
+        return const SoftSkillsSelectionScreen(isEditMode: false);
+      },
+    ),
+    GoRoute(
+      name: 'hobbies',
+      path: '/hobbies',
+      builder: (BuildContext context, GoRouterState state) {
+        return const HobbiesSelectionScreen(
+          isEditMode: false,
+        );
+      },
+    ),
+    GoRoute(
+      name: 'editHobbies',
+      path: '/editHobbies',
+      builder: (BuildContext context, GoRouterState state) {
+        return const HobbiesSelectionScreen(isEditMode: true);
+      },
+    ),
+    GoRoute(
+      name: 'jobSeekerProfile',
+      path: '/jobSeekerProfile',
+      builder: (BuildContext context, GoRouterState state) {
+        return const JobSeekerScreen();
+      },
+    ),
+    GoRoute(
+      name: 'recruiterProfile',
+      path: '/recruiterProfile',
+      builder: (BuildContext context, GoRouterState state) {
+        return const RecruiterScreen();
+      },
+    ),
+    GoRoute(
+      path: '/editSoftSkillsSeeker',
+      builder: (BuildContext context, GoRouterState state) {
+        return const SoftSkillsSelectionScreen(isEditMode: true);
+      },
+    ),
+  ],
+);
