7b896d1cfd652d7e0e5a9871c6935bb169090f8d
Léo CHARTIER <leo.chartier@algosup.com>
2024-06-10 14:42:35 +0200

    Add sign in footer

--- src/adopteuncandidat/lib/sign_in.dart
+++ src/adopteuncandidat/lib/sign_in.dart
@@ -60,6 +60,7 @@ class _SignInScreenState extends State<SignInScreen> {
                     ),
                   ),
                 ),
+                const SignInFooter(),
               ],
             ),
           ),
@@ -109,6 +110,67 @@ class SignInBackground extends StatelessWidget {
           ],
         );
       },
+    );
+  }
+}
+
+class SignInFooter extends StatelessWidget {
+  const SignInFooter({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        // TODO: Animation here
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            SignInFooterSelection(
+              text: 'Recruteur',
+              icon: Icons.business,
+            ),
+            SignInFooterSelection(
+              text: 'Chercheur d\'emploi',
+              icon: Icons.person,
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class SignInFooterSelection extends StatelessWidget {
+  final String text;
+  final IconData icon;
+  const SignInFooterSelection(
+      {required this.text, required this.icon, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Icon(
+              icon,
+              size: 50,
+              color: Colors.white,
+            ), // Increased size of the icon
+            Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(100)),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(text),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
