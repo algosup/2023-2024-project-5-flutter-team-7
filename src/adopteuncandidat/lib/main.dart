d9e93bc5babcd2323bbc5262a46a87283358163e
Robin Debry <91249812+robin-debry@users.noreply.github.com>
2024-06-13 13:58:48 +0200

    Remove the debug mode banner

--- src/adopteuncandidat/lib/main.dart
+++ src/adopteuncandidat/lib/main.dart
@@ -19,6 +19,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
+        debugShowCheckedModeBanner: false,
         routerConfig: router,
       ),
     );
