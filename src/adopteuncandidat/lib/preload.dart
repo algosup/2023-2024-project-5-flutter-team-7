c6485eb4dd5b43925f796e8c2aa3185abf6f496b
Robin Debry <91249812+robin-debry@users.noreply.github.com>
2024-06-13 13:58:28 +0200

    Remove unused package for preload

--- src/adopteuncandidat/lib/preload.dart
+++ src/adopteuncandidat/lib/preload.dart
@@ -1,16 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+
 
 class PreloadedAssets {
   static AssetImage signinCompanyBackground =
       const AssetImage('assets/signin_company.jpg');
   static AssetImage signinSeekerBackground =
       const AssetImage('assets/signin_seeker.jpg');
-
-  static final List<AssetImage> _allAssets = [
-    PreloadedAssets.signinCompanyBackground,
-    PreloadedAssets.signinSeekerBackground,
-  ];
 
   static void preloadAll(BuildContext context) {
     precacheImage(signinCompanyBackground, context);
